06182bf0b5fb9f6c5384f07b4c0b5a91277eca28
Bank5807 <TAnWiPaSa@gmail.com>
2024-02-22 14:22:02 +0700

    add html and csv2txt

--- app.py
+++ app.py
@@ -1,36 +1,55 @@
-from flask import Flask, request, jsonify
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from flask import Flask, request, jsonify, render_template, send_file
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-# Load your fine-tuned PyTorch NLP model
-tokenizer = AutoTokenizer.from_pretrained("clicknext/phayathaibert")
-model = AutoModelForSequenceClassification.from_pretrained(
-    "pytbert_sentiment_analysis")
 
-# Set the model to evaluation mode
-model.eval()
+@app.route('/')
+def upload_file():
+    return render_template('upload.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the text input from the request
-    data = request.get_json()
-    text = data['text']
+    # Check if a file is uploaded
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'})
 
-    # Tokenize the text
-    inputs = tokenizer(text, return_tensors="pt",
-                       padding=True, truncation=True)
+    file = request.files['file']
 
-    # Perform inference
-    with torch.no_grad():
-        outputs = model(**inputs)
+    # Check if the file is empty
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'})
 
-    # Get the predicted class
-    predicted_class = torch.argmax(outputs.logits).item()
+    # Check if the file format is supported
+    if not file.filename.endswith(('.csv', '.xlsx')):
+        return jsonify({'error': 'Unsupported file format'})
 
-    return jsonify({'predicted_class': predicted_class})
+    # Read the file
+    if file.filename.endswith('.csv'):
+        df = pd.read_csv(file)
+    else:
+        df = pd.read_excel(file)
+
+    # Get the filename without extension
+    filename = os.path.splitext(file.filename)[0]
+
+    # Export non-empty and non-NaN sentences to a text file
+    export_filename = f"{filename}.txt"
+    with open(export_filename, 'w', encoding='utf-8') as f:
+        for index, row in df.iterrows():
+            # Assuming the sentences are in the first column
+            sentence = row.iloc[8]
+            if pd.notna(sentence) and sentence.strip():  # Skip NaN and empty sentences
+                f.write("%s\n" % sentence)
+
+    return render_template('download.html', filename=export_filename, original_filename=filename)
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def download(filename):
+    return send_file(filename, as_attachment=True)
 
 
 if __name__ == '__main__':
